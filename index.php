1fbb10381aae9686af1f72745f4280027d736e02
Barthélémy Drabczuk <barthelemy.drabczuk@etu.univ-amu.fr>
2018-11-27 22:13:03 +0100

    remise du projet à zéro

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: percevase
- * Date: 21/10/18
- * Time: 17:31
- */
-
-
-
-?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="style.css">
+		<title>Tengjordin</title>
+	</head>
+	<body>
+		<nav>
+			<table>
+				<li>
+					<ul><a href="index.php">Accueil</a></ul>
+					<ul><a href="univers.php">Univers</a></ul>
+					<ul><a href="jdr.php">JDR</a></ul>
+				</li>
+			</table>
+		</nav>
+		<div id="page-content">
+			<article>
+				<h1>Bonjour à tous</h1>
+				<div>bonjour Atous, Portous et Aramous</div>
+			</article>
+		</div>
+	</body>
+</html>
