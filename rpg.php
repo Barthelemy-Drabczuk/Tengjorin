0f273d5ff23105137680b130c89f8877610d96d2
DRABCZUK Barthelemy <d17010509@a-maq-iutinfo.salsa.univ-amu.fr>
2018-11-28 08:46:55 +0100

    ajout de la dernière page importante

---
+++ rpg.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: d17010509
+ * Date: 28/11/18
+ * Time: 08:35
+ */
+
+require_once "page.utiles.php";
+start_page();
+?>
+            <table>
+                <tr>
+                    <th>Introduction</th>
+                    <th>Classes</th>
+                    <th>Nationalités</th>
+                    <th>Objets</th>
+                    <th>Bestitaire</th>
+                </tr>
+                <tr>
+                    <td>Wallah</td>
+                    <td>Forteresse</td>
+                    <td>Le pays du feu</td>
+                    <td>LA BOULE MAGIQUE</td>
+                    <td>La sale bête</td>
+                </tr>
+            </table>
+<?php
+    end_page();
+?>
--- univers.php
+++ univers.php
@@ -3,15 +3,20 @@
 	start_page();
 ?>
 
-<table>
-	<tr>
-		<th>Personnages importants</th>
-		<th>Histoire</th>
-		<th>Peuple</th>
-	</tr>
-	<tr>
-		<td>Wallah</td>
-		<td>La bonne histoire</td>
-		<td>Akhab</td>
-	</tr>
-</table>
+            <table>
+                <tr>
+                    <th>Personnages importants</th>
+                    <th>Histoire</th>
+                    <th>Peuple</th>
+                </tr>
+                <tr>
+                    <td>Wallah</td>
+                    <td>La bonne histoire</td>
+                    <td>Akhab</td>
+                </tr>
+            </table>
+<?php
+
+    end_page();
+
+?>
